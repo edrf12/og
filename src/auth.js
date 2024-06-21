d661c853e54eff3ce6d28886cf8f4365cc9657fa
edrf12 <me@edrf.tech>
2024-06-20 21:18:09 -0300

    Trust host

--- src/auth.js
+++ src/auth.js
@@ -2,5 +2,6 @@ import { SvelteKitAuth } from "@auth/sveltekit";
 import Discord from "@auth/sveltekit/providers/discord";
 
 export const { handle } = SvelteKitAuth({
+	trustHost: true,
 	providers: [Discord],
 });
